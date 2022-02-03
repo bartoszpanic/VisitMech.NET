77058c12890665a59bda8f405ae438bb1534995f
bartoszpanic <74921751+bartoszpanic@users.noreply.github.com>
2022-02-03 12:19:18 +0100

    Add next entities

---
+++ VisitMech.NET.Domain/Entities/Mechanic.cs
@@ -0,0 +1,11 @@
+﻿namespace VisitMech.NET.Domain.Entities
+{
+    public class Mechanic : AuditableEntity
+    {
+        public int MechanicId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int MechanicalWorkShopId { get; set; }
+        public MechanicalWorkShop MechanicalWorkShop { get; set; }
+    }
+}
---
+++ VisitMech.NET.Domain/Entities/MechanicalWorkShop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitMech.NET.Domain.Entities
+{
+    public class MechanicalWorkShop : AuditableEntity
+    {
+        public int MechanicalWorkShopId { get; set; }
+        public string Name { get; set; }
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+        public List<Mechanic> Mechanic { get; set; }
+    }
+}
